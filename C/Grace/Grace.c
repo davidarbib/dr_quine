702a3e604f3e0d6e1437103cc82b56a7d06003e5
David Arbib <davidarbib@yahoo.fr>
2025-01-01 17:19:20 +0100

    Grace done

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -4,6 +4,7 @@
     What a comment !
  */
 
-#define FILENAME "Grace_kid.c"
-#define FT(b)int main(){puts("#include <stdio.h>");puts("");puts(" /*");puts("    What a comment !");puts(" */");puts("");puts(b);puts("FT("#b")");return 0;}
-FT("#define FT(b)int main(){puts(\"#include <stdio.h>\");puts(\"\");puts(\" /*\");puts(\"    What a comment !\");puts(\" */\");puts(\"\");puts(b);puts(\"FT(\"#b\")\");return 0;}")
+#define ADDLF()fputc(10, handler);
+#define OPEN(f)FILE *handler = fopen(f, "w");if(!handler){return 1;}
+#define FT(b)int main(){OPEN("Grace_kid.c")fputs("#include <stdio.h>", handler);ADDLF()ADDLF()fputs(" /*", handler);ADDLF()fputs("    What a comment !", handler);ADDLF()fputs(" */", handler);ADDLF()ADDLF()fputs("#define ADDLF()fputc(10, handler);", handler);ADDLF()fputs("#define OPEN(f)FILE *handler = fopen(f, \"w\");if(!handler){return 1;}", handler);ADDLF()fputs(b, handler);ADDLF()fputs("FT("#b")", handler);ADDLF()fclose(handler);return 0;}
+FT("#define FT(b)int main(){OPEN(\"Grace_kid.c\")fputs(\"#include <stdio.h>\", handler);ADDLF()ADDLF()fputs(\" /*\", handler);ADDLF()fputs(\"    What a comment !\", handler);ADDLF()fputs(\" */\", handler);ADDLF()ADDLF()fputs(\"#define ADDLF()fputc(10, handler);\", handler);ADDLF()fputs(\"#define OPEN(f)FILE *handler = fopen(f, \\\"w\\\");if(!handler){return 1;}\", handler);ADDLF()fputs(b, handler);ADDLF()fputs(\"FT(\"#b\")\", handler);ADDLF()fclose(handler);return 0;}")
