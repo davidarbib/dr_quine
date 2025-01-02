d314cb1dc401da094bfae8847732a69fb6c60bc4
David Arbib <davidarbib@yahoo.fr>
2025-01-02 23:46:44 +0100

    todo find a trick to handle i value and source/exe names generation

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -1,42 +1,58 @@
-#ifdef I
-# if(I == 5)
-int i = 5;
-#  undef I
-#  define I 4
-# elif(I == 4)
-int i = 4;
-#  undef I
-#  define I 3
-# elif(I == 3)
-int i = 3;
-#  undef I
-#  define I 2
-# elif(I == 2)
-int i = 2;
-#  undef I
-#  define I 1
-# elif(I == 1)
-int i = 1;
-#  undef I
-#  define I 0
-# else
-int i = 0;
-#  undef I
-# endif
-#else
-# define I 5 
-int i = 5;
-#endif
-
 #include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+#define CC "gcc"
+#define CMDSIZE 100
+#define SULLYSIZE 6
+#define NAMESIZE 15
+#define INITSIZE 15
 #define SULLYNAME "Sully_"
-#define CAT(a,b)a##b
+#define DIGIT(x)'0'+x
 #define ADDLF()fputc(10, handler);
-#define PRINT_DECLARE_I(d)fputs(CAT("int i = ", d), handler);fputs(";");ADDLF()
-#define CHECK_
-
-#define COMPILE(i)system("gcc -o  ");
+#define PRINT_DECLARE_I()fputs(initbuf, handler);ADDLF()
 #define OPEN(f)FILE *handler = fopen(f, "w");if(!handler){return 1;}
+
+int i = 5;
+
+//beware of i value when making files
+//
+void make_exe_name(char *buf)
+{
+bzero(buf, NAMESIZE);
+strcpy(buf, SULLYNAME);
+buf[SULLYSIZE] = DIGIT(i);
+}
+
+void make_src_name(char *buf)
+{
+make_exe_name(buf);
+strcat(buf, ".c");
+}
+
+void make_declare_str(char *buf)
+{
+bzero(buf, INITSIZE);
+strcpy(buf, "int i = ");
+buf[strlen(buf)] = DIGIT(i);
+strcat(buf, ";");
+}
+
+void make_compile_command(char* buf)
+{
+char srcbuf[NAMESIZE];
+char exebuf[NAMESIZE];
+bzero(buf, INITSIZE);
+strcpy(buf, CC);
+strcat(buf, " -o ");
+make_exe_name(exebuf);
+make_src_name(srcbuf);
+strcat(buf, exebuf);
+strcat(buf, " ");
+strcat(buf, srcbuf);
+}
+
 #define FT(b)int main(){OPEN("Grace_kid.c")fputs("#include <stdio.h>", handler);ADDLF()ADDLF()fputs(" /*", handler);ADDLF()fputs("    What a comment !", handler);ADDLF()fputs(" */", handler);ADDLF()ADDLF()fputs("#define ADDLF()fputc(10, handler);", handler);ADDLF()fputs("#define OPEN(f)FILE *handler = fopen(f, \"w\");if(!handler){return 1;}", handler);ADDLF()fputs(b, handler);ADDLF()fputs("FT("#b")", handler);ADDLF()fclose(handler);return 0;}
 FT("#define FT(b)int main(){OPEN(\"Grace_kid.c\")fputs(\"#include <stdio.h>\", handler);ADDLF()ADDLF()fputs(\" /*\", handler);ADDLF()fputs(\"    What a comment !\", handler);ADDLF()fputs(\" */\", handler);ADDLF()ADDLF()fputs(\"#define ADDLF()fputc(10, handler);\", handler);ADDLF()fputs(\"#define OPEN(f)FILE *handler = fopen(f, \\\"w\\\");if(!handler){return 1;}\", handler);ADDLF()fputs(b, handler);ADDLF()fputs(\"FT(\"#b\")\", handler);ADDLF()fclose(handler);return 0;}")
