b549cd8938807da3a4bd4fcef638f27743792e35
David ARBIB <darbib@paul-f4Br5s1.clusters.42paris.fr>
2025-01-03 20:02:20 +0100

    wip

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -14,60 +14,94 @@
 #define ADDLF()fputc(10, handler);
 #define PRINT_DECLARE_I()fputs(initbuf, handler);ADDLF()
 #define OPEN(f)FILE *handler = fopen(f, "w");if(!handler){return 1;}
+#define TEST()i == 5 && access("Sully_5.c", F_OK) == -1
+#define FPUTS(s)fputs(s, handler);
 
 int i = 5;
 
-//beware of i value when making files
-//
-void make_exe_name(char *buf)
-{
+void make_exe_name(char *buf, int a){
 bzero(buf, NAMESIZE);
 strcpy(buf, SULLYNAME);
-buf[SULLYSIZE] = DIGIT(i);
-}
+buf[SULLYSIZE] = DIGIT(a);}
 
-void make_src_name(char *buf)
-{
-make_exe_name(buf);
-strcat(buf, ".c");
-}
+void make_src_name(char *buf, int a){
+make_exe_name(buf, a);
+strcat(buf, ".c");}
 
-void make_declare_str(char *buf, int a)
-{
+void make_declare_str(char *buf, int a){
 bzero(buf, INITSIZE);
 strcpy(buf, "int i = ");
 buf[strlen(buf)] = DIGIT(a);
-strcat(buf, ";");
-}
+strcat(buf, ";");}
 
-void make_compile_command(char* buf)
-{
-char srcbuf[NAMESIZE];
-char exebuf[NAMESIZE];
+void make_compile_command(char* buf, int a){
+char srcbuf[NAMESIZE];char exebuf[NAMESIZE];
 bzero(buf, INITSIZE);
-strcpy(buf, CC);
-strcat(buf, " -o ");
-make_exe_name(exebuf);
-make_src_name(srcbuf);
-strcat(buf, exebuf);
-strcat(buf, " ");
-strcat(buf, srcbuf);
-}
+strcpy(buf, CC);strcat(buf, " -o ");
+make_exe_name(exebuf, a);make_src_name(srcbuf, a);
+strcat(buf, exebuf);strcat(buf, " ");strcat(buf, srcbuf);}
 
-void choose_to_clone()
+void make_launch_command(char* buf, int a){
+bzero(buf, NAMESIZE);
+strcpy(buf, "./"); strcat(buf, SULLYNAME);
+buf[strlen(buf)] = DIGIT(a);}
+
+int choose_to_clone()
 {
-    char declare_buf[INITSIZE];
-    if (i == 5 && access("Sully_5.c", F_OK) == -1)
+    char declare_buf[INITSIZE]; char src_buf[NAMESIZE]; char cmd_buf[CMDSIZE]; char launch_buf[CMDSIZE]; FILE *handler = NULL;
+    int a = (TEST())? i : i-1;
+    if (i >= 0)
     {
-        make_declare_str(declare_buf, i);
+        make_src_name(src_buf, a);
+        OPEN(src_buf);
+        FPUTS("#include <stdio.h>")ADDLF()
+        FPUTS("#include <string.h>")ADDLF()
+        FPUTS("#include <strings.h>")ADDLF()
+        FPUTS("#include <unistd.h>")ADDLF()
+        FPUTS("#include <stdlib.h>")ADDLF()ADDLF()
+        FPUTS("#define CC \"gcc\"")ADDLF()
+        FPUTS("#define CMDSIZE 100")ADDLF()
+        FPUTS("#define SULLYSIZE 6")ADDLF()
+        FPUTS("#define NAMESIZE 15")ADDLF()
+        FPUTS("#define INITSIZE 15")ADDLF()
+        FPUTS("#define SULLYNAME \"Sully_\"")ADDLF()
+        FPUTS("#define DIGIT(x)'0'+x")ADDLF()
+        FPUTS("#define ADDLF()fputc(10, handler);")ADDLF()
+        FPUTS("#define PRINT_DECLARE_I()fputs(initbuf, handler);ADDLF()")ADDLF()
+        FPUTS("#define OPEN(f)FILE *handler = fopen(f, \"w\");if(!handler){return 1;}")ADDLF()
+        FPUTS("#define TEST()i == 5 && access(\"Sully_5.c\", F_OK) == -1")ADDLF()ADDLF()
+        FPUTS("#define FPUTS(s)fputs(s, handler);")ADDLF()
+
+        make_declare_str(declare_buf, a);
+        FPUTS(declare_buf)ADDLF()ADDLF()
+FPUTS("void make_exe_name(char *buf, int a){")ADDLF()
+FPUTS("bzero(buf, NAMESIZE);")ADDLF()
+FPUTS("strcpy(buf, SULLYNAME);")ADDLF()
+FPUTS("buf[SULLYSIZE] = DIGIT(a);}")ADDLF()ADDLF()
+FPUTS("void make_src_name(char *buf, int a){")ADDLF()
+FPUTS("make_exe_name(buf, a);")ADDLF()
+FPUTS("strcat(buf, \".c\");}")ADDLF()ADDLF()
+FPUTS("void make_declare_str(char *buf, int a){")ADDLF()
+FPUTS("bzero(buf, INITSIZE);")ADDLF()
+FPUTS("strcpy(buf, \"int i = \");")ADDLF()
+FPUTS("buf[strlen(buf)] = DIGIT(a);")ADDLF()
+FPUTS("strcat(buf, \";\");}")ADDLF()ADDLF()
+FPUTS("void make_compile_command(char* buf, int a){")ADDLF()
+FPUTS("char srcbuf[NAMESIZE];char exebuf[NAMESIZE];")ADDLF()
+FPUTS("bzero(buf, INITSIZE);")ADDLF()
+FPUTS("strcpy(buf, CC);strcat(buf, \" -o \");")ADDLF()
+FPUTS("make_exe_name(exebuf, a);make_src_name(srcbuf, a);")ADDLF()
+FPUTS("strcat(buf, exebuf);strcat(buf, \" \");strcat(buf, srcbuf);}")ADDLF()ADDLF()
+FPUTS("void make_launch_command(char* buf, int a){")ADDLF()
+FPUTS("bzero(buf, NAMESIZE);")ADDLF()
+FPUTS("strcpy(buf, \"./\"); strcat(buf, SULLYNAME);")ADDLF()
+FPUTS("buf[strlen(buf)] = DIGIT(a);}")ADDLF()
+        make_compile_command(cmd_buf, a);
+        make_launch_command(launch_buf, a);
+        system(cmd_buf);
+        system(launch_buf);
     }
-    else if (i >= 0)
-    {
-        make_declare_str(declare_buf, i - 1);
-    }
-    //print code source of child program
-    //compile
-    //execute
+    return 0;
 }
 
 #define FT(b)int main(){OPEN("Grace_kid.c")fputs("#include <stdio.h>", handler);ADDLF()ADDLF()fputs(" /*", handler);ADDLF()fputs("    What a comment !", handler);ADDLF()fputs(" */", handler);ADDLF()ADDLF()fputs("#define ADDLF()fputc(10, handler);", handler);ADDLF()fputs("#define OPEN(f)FILE *handler = fopen(f, \"w\");if(!handler){return 1;}", handler);ADDLF()fputs(b, handler);ADDLF()fputs("FT("#b")", handler);ADDLF()fclose(handler);return 0;}
