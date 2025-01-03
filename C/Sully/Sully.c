bfb40ae580f7644ae2826a3bae28df79a7a541f6
David ARBIB <darbib@paul-f4Br5s1.clusters.42paris.fr>
2025-01-03 17:46:11 +0100

    ongoing conditionnal generation of child program source code

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -32,11 +32,11 @@ make_exe_name(buf);
 strcat(buf, ".c");
 }
 
-void make_declare_str(char *buf)
+void make_declare_str(char *buf, int a)
 {
 bzero(buf, INITSIZE);
 strcpy(buf, "int i = ");
-buf[strlen(buf)] = DIGIT(i);
+buf[strlen(buf)] = DIGIT(a);
 strcat(buf, ";");
 }
 
@@ -52,6 +52,22 @@ make_src_name(srcbuf);
 strcat(buf, exebuf);
 strcat(buf, " ");
 strcat(buf, srcbuf);
+}
+
+void choose_to_clone()
+{
+    char declare_buf[INITSIZE];
+    if (i == 5 && access("Sully_5.c", F_OK) == -1)
+    {
+        make_declare_str(declare_buf, i);
+    }
+    else if (i >= 0)
+    {
+        make_declare_str(declare_buf, i - 1);
+    }
+    //print code source of child program
+    //compile
+    //execute
 }
 
 #define FT(b)int main(){OPEN("Grace_kid.c")fputs("#include <stdio.h>", handler);ADDLF()ADDLF()fputs(" /*", handler);ADDLF()fputs("    What a comment !", handler);ADDLF()fputs(" */", handler);ADDLF()ADDLF()fputs("#define ADDLF()fputc(10, handler);", handler);ADDLF()fputs("#define OPEN(f)FILE *handler = fopen(f, \"w\");if(!handler){return 1;}", handler);ADDLF()fputs(b, handler);ADDLF()fputs("FT("#b")", handler);ADDLF()fclose(handler);return 0;}
