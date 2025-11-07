31eea5c46bcb1dd7ed42b6ace9fe72039309da75
David ARBIB <darbib@f1r12s21.clusters.42paris.fr>
2025-11-07 18:08:17 +0100

    wip

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -16,6 +16,8 @@ char *tape[] = {
 "Sully_",
 ".c",
 "clang -Wall -Werror -Wextra",
+"%s",
+"%c%s%c",
 "#include <stdio.h>",
 "#include <string.h>",
 "#include <strings.h>",
@@ -27,18 +29,26 @@ char *tape[] = {
 "#define NAMESIZE 15",
 "#define INITSIZE 15",
 "int i = 5;",
+"char *tape[] = {",
+"};",
 "int main(){",
 "}"
 };
 
 int main(){
+    char lf[1] = {74};
     int a = i - 1;
-    char buf[SULLYSIZE];
-    bzero(buf, SULLYSIZE);
+    char buf[SULLYSIZE + 3];
+
+    FILE *handler = fopen(buf, tape[0]);if(!handler){return 1;}
+    bzero(buf, SULLYSIZE + 3);
     strcpy(buf, tape[1]);
-    strcat()
-    FILE *handler = fopen(f, tape[0]);if(!handler){return 1;}
-    for (int i = 0; i <= )
-
-
+    buf[SULLYSIZE] = DIGIT(a);
+    strcat(buf, tape[2]);
+    for (int i = 6; i < 11; i++){
+        fprintf(handler, tape[4], tape[i]);
+        fwrite(lf, 1, 1, handler);
+    }
+    fwrite(lf, 1, 1, handler);
+    fprintf(handler, tape[4], tape[16]);
 }
